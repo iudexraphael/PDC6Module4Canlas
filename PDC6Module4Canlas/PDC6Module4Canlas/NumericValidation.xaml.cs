893ad87bc59a4d6a246a6f6013d468c86cb60c6a
Joseph Canlas <49482845+iudexraphael@users.noreply.github.com>
2021-09-16 21:21:37 +0800

    Add project files.

---
+++ PDC6Module4Canlas/PDC6Module4Canlas/NumericValidation.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC6Module4Canlas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Gesture : ContentPage
+    {
+        public Gesture()
+        {
+            InitializeComponent();
+        }
+    }
+}
